e335f35d5ac7f837fbfc761f9990ef0c7f0e6b26
Sasikala <sashipemmanaboidi@gmail.com>
2022-06-20 19:40:40 +0530

    amazon

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,19 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAlNu0hksWPZkN1P0mcKOwumtnGmfT96K4",
-  authDomain: "clone-31c25.firebaseapp.com",
-  projectId: "clone-31c25",
-  storageBucket: "clone-31c25.appspot.com",
-  messagingSenderId: "115596257673",
-  appId: "1:115596257673:web:0878f63f23972a00365958",
-  measurementId: "G-SEE4P00GSB"
-};
 
-const firebaseApp = firebase.initalizeApp(firebaseConfig);
-const db = firebaseApp.firestore();
+ const firebaseConfig = {
+    // paste your firebase config object here
+        apiKey: "AIzaSyAlNu0hksWPZkN1P0mcKOwumtnGmfT96K4",
+        authDomain: "clone-31c25.firebaseapp.com",
+        projectId: "clone-31c25",
+        storageBucket: "clone-31c25.appspot.com",
+        messagingSenderId: "115596257673",
+        appId: "1:115596257673:web:0878f63f23972a00365958",
+        measurementId: "G-SEE4P00GSB"
+      };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
 const auth = firebase.auth();
-
-export {db, auth};
+const db = firebaseApp.firestore();
+export { auth };
+export {db};
